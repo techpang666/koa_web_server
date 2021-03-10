7a96e7515345c85c56b6492078a87e17d26a9aaf
佛系玩家沈公子 <techpang666@gmail.com>
2021-03-11 03:19:03 +0800

    fix_repo

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // 创建服务器实例
 const Koa = require('koa')
-const serve = require('koa-static')
 const app = new Koa()
 
-// 利用静态插件, 将 Public 下面的文件提供访问服务
+// 加载koa的静态插件
+const serve = require('koa-static')
+// 利用静态插件 渲染public目录 提供页面服务
 app.use(serve(__dirname + '/public'))
 
-// 开启服务器即可
+// 开启服务器
 app.listen(3333, () => {
-	console.log('服务器已经正常开启, 访问 http://127.0.0.1:3333')
+	console.log('服务器已启动')
 })
